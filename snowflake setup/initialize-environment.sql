70c34e3d614f3b52cc1632b4cfbe10c04ca5601f
Rob <110133091+popsrdg@users.noreply.github.com>
2023-04-17 17:29:04 -0400

    end of day sync

--- snowflake setup/initialize-environment.sql
+++ snowflake setup/initialize-environment.sql
@@ -3,7 +3,6 @@
 -- needed to allow dbt to connect
 create NETWORK POLICY IDENTIFIER('"DBT"') ALLOWED_IP_LIST = ( '52.45.144.63' , '54.81.134.249' , '52.22.161.231' ) COMMENT = '';
 
-
 USE ROLE ACCOUNTADMIN;
 
 /*
@@ -12,24 +11,28 @@ drop user RDG_LOAD_USER;
 drop role rdg_see_all;
 */
 
-
+---------------------------------------------------------------
+-- from accountadmin
 create DATABASE IDENTIFIER('RDG_SANDBOX') COMMENT = '';
+
+create role rdg_see_all;
+
+GRANT OPERATE ON WAREHOUSE compute_wh TO ROLE rdg_see_all;
+GRANT USAGE ON DATABASE rdg_sandbox TO ROLE rdg_see_all;
+GRANT USAGE ON future SCHEMAS IN DATABASE rdg_sandbox TO ROLE rdg_see_all;
+GRANT SELECT,INSERT,UPDATE,DELETE ON future TABLES IN DATABASE rdg_sandbox TO ROLE rdg_see_all;
+
+--GRANT ROLE rdg_see_all TO ROLE sysadmin;
+GRANT ROLE IDENTIFIER('rdg_see_all') TO USER IDENTIFIER('papag');
+GRANT ROLE IDENTIFIER('rdg_see_all') TO USER IDENTIFIER('kumar');
+
 create SCHEMA IDENTIFIER('RDG_SANDBOX.STAGE_01') COMMENT = '';
 create SCHEMA IDENTIFIER('RDG_SANDBOX.T0_SRC_01') COMMENT = '';
 
--- from accountadmin
-create role rdg_see_all;
 
-GRANT ownership ON DATABASE rdg_sandbox TO ROLE rdg_see_all;
-GRANT OPERATE ON WAREHOUSE compute_wh TO ROLE rdg_see_all;
---GRANT USAGE ON DATABASE rdg_sandbox TO ROLE rdg_see_all;
---GRANT USAGE ON ALL SCHEMAS IN DATABASE rdg_sandbox TO ROLE rdg_see_all;
 --GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN DATABASE rdg_sandbox TO ROLE rdg_see_all;
 
-
-GRANT ROLE rdg_see_all TO ROLE sysadmin;
-GRANT ROLE IDENTIFIER('rdg_see_all') TO USER IDENTIFIER('papag');
-GRANT ROLE IDENTIFIER('rdg_see_all') TO USER IDENTIFIER('kumar');
+---------------------------------------------------------------
 
 create USER IDENTIFIER('RDG_LOAD_USER') 
     COMMENT = '' PASSWORD = '☺☺☺☺☺☺☺☺' 
@@ -41,6 +44,12 @@ create USER IDENTIFIER('RDG_LOAD_USER')
 ;
 
 
+---------------------------------------------------------------
+use role rdg_see_all;
 
 
+GRANT OPERATE ON WAREHOUSE compute_wh TO ROLE public;
+GRANT USAGE ON DATABASE rdg_sandbox TO ROLE public;
+GRANT USAGE ON future SCHEMAS IN DATABASE rdg_sandbox TO ROLE public;
+GRANT SELECT,INSERT,UPDATE,DELETE ON future TABLES IN DATABASE rdg_sandbox TO ROLE public;
 
